11c2dd86b47c157c389cf0a6707eb76529a1c672
niturawnak <32084346+niturawnak@users.noreply.github.com>
2022-10-31 16:23:05 +0100

    Add files via upload

---
+++ build/CMakeFiles/OpenCV_naive_stereo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenCV_naive_stereo.dir/src/main.cpp.o"
+  "OpenCV_naive_stereo"
+  "OpenCV_naive_stereo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenCV_naive_stereo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
